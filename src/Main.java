54c214b1225dde4f19c236924ccb14e4a262721b
camlopes <camalopes.cat@gmail.com>
2025-03-21 11:32:16 -0300

    Logica para calcular o valor dos dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
+        int quantidadeDados;
+        int total = 0;
+        int faceDoDado;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        System.out.print("Escolha a quantidade de dados jogados: ");
+        quantidadeDados = scanner.nextInt();
+
+        if(quantidadeDados <= 0){
+            System.out.println("A quantidade de dados não pode ser negativa ou 0.");
+        } else {
+
+            for(int i = 0; i < quantidadeDados; i++){
+                faceDoDado = random.nextInt(1, 7);
+                total += faceDoDado;
+
+                System.out.println("Voce jogou: " + faceDoDado);
+            }
+
+            System.out.println("Soma das faces dos dados jogados: " + total);
+        }
+
+        scanner.close();
     }
 }
